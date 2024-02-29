825d979524558ef637e29ffe3d98a0d00ffe6ae3
hyewon <gpdnjs154@navr.com>
2024-02-29 22:16:05 +0900

    [add]list component 생성

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,50 @@
+import { useDispatch, useSelector } from "react-redux";
+import { TodosState, deleteTodo } from "../store/modules/todos";
+type Todolist = {
+  todos: TodosState;
+};
 const List = () => {
-  return <div>List</div>;
+  const todoList = useSelector((state: Todolist) => state.todos.todos);
+  const dispatch = useDispatch();
+  const deletehandle = (id: number) => {
+    dispatch(deleteTodo(id));
+  };
+  return (
+    <ul style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+      {todoList.map((todo) => {
+        return (
+          <li
+            key={todo.id}
+            style={{
+              display: "flex",
+              gap: "5px",
+              width: "400px",
+              alignItems: "center",
+              border: "2px solid gray",
+              justifyContent: "space-between",
+              padding: "20px",
+            }}
+          >
+            <h1
+              style={{
+                whiteSpace: "normal",
+                wordWrap: "break-word",
+                maxWidth: "340px",
+              }}
+            >
+              {todo.text}
+            </h1>
+            <button
+              onClick={() => deletehandle(todo.id)}
+              style={{ height: "30px" }}
+            >
+              delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default List;
